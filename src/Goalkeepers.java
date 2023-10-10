5cb974475bf5ea16f4fe4b7a7480fefcb3854bef
DELL <isamadesmond@gmail.com>
2023-10-10 04:39:01 -0700

    initial commit

---
+++ src/Goalkeepers.java
@@ -0,0 +1,10 @@
+public class Goalkeepers {
+    private string name;
+    private int age;
+    private int position;
+    private string wing;
+    private char sex;
+    private 
+    private char address;
+
+}
